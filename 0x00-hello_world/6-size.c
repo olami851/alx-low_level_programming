62050563543127ffd45e28571f4c1b1ae6fef87f
olami851 <abdhamid851@gmail.com>
2022-09-13 13:43:01 +0000

    computer sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 
 /**
-*main - Print the various string in a function
+*main - Print the various types of string function
 *
-*Description:using the main function
+*Description: using the main function
 *
-*this program prints the size of various types of computer it compile
+*this program prints the types of computer
 *
 *Return: 0
 */
-int main(0)
+int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
+	 char c;
+        int i;
+        long li;
+        long long lli;
+        float f;
 
-	printf("Size of a char: %ld byte(s)\n", sizeof(c));
-	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+        printf("Size of a char: %ld byte(s)\n", sizeof(c));
+        printf("Size of an int: %ld byte(s)\n", sizeof(i));
+        printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+        printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+        printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	return (0);
 }
