f0e6d75aefc19ea89c1b4e65c546418a7969a305
olami851 <abdhamid851@gmail.com>
2022-09-13 23:41:09 +0000

    Putchar sommand

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*
+*main - Print the string in the putschar function
+*
+*Description: using the main function
+*
+*this program will print _putchar
+*
+*Return: 0
+*/
+int main(void)
+{
+	printf("_putchar_putchar\n");
+	return (0);
+}
