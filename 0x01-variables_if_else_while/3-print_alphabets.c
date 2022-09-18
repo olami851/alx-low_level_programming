c90c0fe993dc38062a2bdbabfcca529125ef7e11
olami851 <abdhamid851@gmail.com>
2022-09-18 02:38:38 -0700

    print lower, upper case alphabets with new line

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 /**
-*main - print lowercase then uppercase numbers followed by a new line
+*main - print lower and upper case with new line 
 *
 *Description: using the main function
-*this program prints "Programming prints lower and upper case alphabets
+*this program prints "Programming that prints lower and upper case alphabets
 *
 *Return: 0
 */
 int main(void)
-{char ch;
+{
+char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-        putchar(ch);
+	putchar(ch);
 }
 for (ch = 'A' ; ch <= 'Z' ; ch++)
 {
-        putchar(ch);
+	putchar(ch);
 }
 putchar('\n');
 return (0);
