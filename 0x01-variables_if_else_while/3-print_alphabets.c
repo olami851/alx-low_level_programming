83476c4b29e11f91c22aa2032f3b4e435a6a8c66
olami851 <abdhamid851@gmail.com>
2022-09-18 01:34:13 -0700

    print lowercase followed by uppercase and then new line

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,7 @@
 *main - print lowercase then uppercase numbers followed by a new line
 *
 *Description: using the main function
-*this program prints "Programming that first prints lowercase then uppercase number and new line
+*this program prints "Programming prints lower and upper case alphabets
 *
 *Return: 0
 */
@@ -11,11 +11,11 @@ int main(void)
 {char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-	putchar(ch);
+        putchar(ch);
 }
 for (ch = 'A' ; ch <= 'Z' ; ch++)
 {
-	putchar(ch);
+        putchar(ch);
 }
 putchar('\n');
 return (0);
